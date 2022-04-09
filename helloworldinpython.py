f87708b82e7f4b9ebb98eb4b025fa303d41d34a3
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-04-09 10:57:16 +0530

    Update helloworldinpython.py

--- helloworldinpython.py
+++ helloworldinpython.py
@@ -1,4 +1,4 @@
-#yes its a great start 
+# yes its a great start 
 # Python is an interpreted, object-oriented,  high-level programming language with dynamic semantics.
 #  Its high-level built in data structures, combined with dynamic typing and dynamic binding,
 #  make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language...
