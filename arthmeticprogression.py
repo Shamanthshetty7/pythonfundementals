54b0a83f7924547ed43dddf8f49a75f16d303072
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-04-13 13:54:10 +0530

    added new file
    finding arithemetic progression

---
+++ arthmeticprogression.py
@@ -0,0 +1,19 @@
+# Arithemetic Progression
+# You are given first three entries of an arithmetic progression. You have to calculate the common difference and print it.
+#Sample Input:
+# 1
+# 3
+# 5
+# Sample Output:
+# 2
+
+# here we are caluculating common difference of the numbers
+a=int(input())
+b=int(input())
+c=int(input())
+com=b-a or c-b        # logical operaters are used commonly like  'or' ,'and'  .....
+print(com)
+
+
+# that's it....
+# lets move to next program..
