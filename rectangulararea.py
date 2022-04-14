46edc531b48abc5daecdbbac93ee31f1688fd920
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-04-14 12:23:35 +0530

    added new file
    finding area of the rectangular

---
+++ rectangulararea.py
@@ -0,0 +1,26 @@
+# Finding Rectangular Area
+
+# You are given a rectangle in a plane. The coordinates of one of its diagonals are provided to you.
+# You have to print the total area of the rectangle.
+# The coordinates of the rectangle are provided as four integral values: x1, y1, x2, y2. It is given that x1 < x2 and y1 < y2.
+
+# Sample Input:
+# 1
+# 1
+# 3
+# 3
+# Sample Output:
+# 4
+
+# here is your code
+x1=int(input())
+y1=int(input())
+x2=int(input())
+y2=int(input())
+area=(x2-x1)*(y2-y1)   # formula of rectangular area
+print(area)
+
+# great going....
+
+# let us move to next program..
+# we will be working on conditions and loops in upcoming programs.
