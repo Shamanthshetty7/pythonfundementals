53fb71ba83b67002f1f27b69d39e6af456a2315f
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-04-21 18:37:08 +0530

    palindrome

---
+++ pallindromeofnumber.py
@@ -0,0 +1,24 @@
+# Palindrome number
+
+# Write a program to determine if given number is palindrome or not. Print true if it is palindrome, false otherwise.
+# Palindrome are the numbers for which reverse is exactly same as the original one. For eg. 121
+
+# Sample Input 1 :
+# 121
+# Sample Output 1 :
+# true
+
+# here is the code
+n=int(input())
+temp=n                     # assigning temporary varible to hold the value of n(if required)    
+rev=0      
+while n>0:
+    rem=n%10              # finding the reverse as we already gone through previous program
+    rev=(rev*10)+rem
+    n=n//10
+if(rev==temp):            # comparing reversed value with the original value
+    print("true")         # if true
+else:
+    print('false')        # else
+
+    # and thats it..
