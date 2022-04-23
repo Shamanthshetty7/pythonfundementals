d7ff7c2802101d1fb6f3eddaddd7755c6f6a12a7
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-04-23 12:11:02 +0530

    nthfibonacci

---
+++ nthfibonacci.py
@@ -0,0 +1,26 @@
+# Nth Fibonacci Number
+
+# Nth term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
+# F(n) = F(n-1) + F(n-2), 
+# Where, F(1) = F(2) = 1
+# Provided N you have to find out the Nth Fibonacci Number
+
+# Sample Input 1:
+# 6
+# Sample Output 1:
+# 8
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+n=int(input())
+a=0
+b=1
+c=-1
+for i in range(n):        # using for loop.for i in range(initial condition,finalcondition,increment/decrement).by default initial condition will be zero and increment will be one.
+    c=a+b                 # method pf storing fibbonacci sequences.
+    a=b
+    b=c
+print(a)
+
+# give little effort to understand the concept.
+# thats it.
