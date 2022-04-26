f8596ab9285ef15ad62b171cdd7e8fb791e7a31d
Shamanth S Shetty <86283625+Shamanthshetty7@users.noreply.github.com>
2022-04-26 20:04:30 +0530

    Update infinitezero.py

--- infinitezero.py
+++ infinitezero.py
@@ -1,9 +1,9 @@
-#  Entering infinite loop will through you time limit exceeding errors.
+#  Entering infinite loop will throw you time limit exceeding errors.
 
 i=0
 while i<10:        # while loop
     print(i)       # body of the while loop
-i = i+1            # incrementation outside the loop.There is no connection between leeop body and this line.
+i = i+1            # incrementation outside the loop.There is no connection between loop body and this line.
 
 # program will continously print the value of i which is assigned at  the begining.there is no termination cases.
 # check it through any python ide
