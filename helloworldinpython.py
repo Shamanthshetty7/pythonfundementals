46c8458ecb1e0e9151b5a521be059bab12fda278
Shamanth S Shetty <86283625+Shamanthshetty7@users.noreply.github.com>
2022-04-29 10:16:41 +0530

    Update helloworldinpython.py

--- helloworldinpython.py
+++ helloworldinpython.py
@@ -3,15 +3,15 @@
 #  Its high-level built in data structures, combined with dynamic typing and dynamic binding,
 #  make it very attractive for Rapid Application Development, as well as for use as a scripting or glue language...
 
-#so...let"s begin
+# so...let's begin
 
-print("HELLO WORLD")        #if you want to print anything just use print()
-print(100)                  #" " or ' ' is used inside the print() only if you wanna print character or string
-a=50                        #assignment of values are simpler. just use the variable and  = and the value which you want to store(variable=value)
-b="Shamanth"                #strings are used only within the " " or ' '
-print(a,end=' ')            #if you dont want to give line break then just use end="spaces how much you want" 
+print("HELLO WORLD")        # if you want to print anything just use print()
+print(100)                  # " " or ' ' is used inside the print() only if you wanna print character or string
+a=50                        # assignment of values are simpler. just use the variable and  = and the value which you want to store(variable=value)
+b="Shamanth"                # strings are used only within the " " or ' '
+print(a,end=' ')            # if you dont want to give line break then just use end="spaces how much you want" 
 print(b)
 print("move forword to next program")
 
 
-#just run it over any python ide 
+# just run it over any python ide .
