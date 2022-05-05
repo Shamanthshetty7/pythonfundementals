eb353c54fa3d55a17f3d9669bf4614c63b8a647d
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-05-05 16:53:01 +0530

    Number Pattern 4

---
+++ numbpattern4.py
@@ -0,0 +1,22 @@
+# Number Pattern 4
+
+# Pattern for N = 4
+# 1234
+# 123
+# 12
+# 1
+
+## Read input as specified in the question.
+## Print output as specified in the question.
+n=int(input())
+i=n
+
+while i>=1:
+    j=1
+    while j<=i:
+        print(j,end='')
+        j=j+1
+    print()
+    i=i-1
+    
+# Thats it.
