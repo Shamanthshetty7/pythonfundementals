45121a2122d2403202a4bf686f8103b5162e08de
Shamanth S Shetty <86283625+Shamanthshetty7@users.noreply.github.com>
2022-05-06 09:39:28 +0530

    Update infinitezero.py

--- infinitezero.py
+++ infinitezero.py
@@ -6,4 +6,4 @@ while i<10:        # while loop
 i = i+1            # incrementation outside the loop.There is no connection between loop body and this line.
 
 # program will continously print the value of i which is assigned at  the begining.there is no termination cases.
-# check it through any python ide
+# check it through any python ide.
