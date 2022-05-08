afdb37e2f8defc2f29dbb29fad5e9196197d6d0d
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-05-08 09:42:06 +0530

    Mirror Number Pattern

---
+++ mirrornumpattern.py
@@ -0,0 +1,29 @@
+# Code : Mirror Number Pattern
+
+# Pattern for N = 4
+
+# ...1
+# ..12
+# .123
+# 1234
+
+# in the place of dot(.) print spaces.
+
+## Read input as specified in the question
+## Print the required output in given format
+n=int(input())
+i=1
+while i<=n:
+    s=1
+    while s<=n-i:
+        print(' ',end='')           # printing spaces seperately using loop and then  simultaneously number pattern
+        s=s+1
+    j=1
+    while j<=i:
+        print(j,end='')
+        j=j+1
+    print()
+    i=i+1
+
+# Done.
+
