e460f67a292574206cb800fe6edbe4cd7fcd592f
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-05-15 23:25:33 +0530

    arrow pattern

---
+++ arrowpattern.py
@@ -0,0 +1,42 @@
+# Arrow pattern
+
+# Note : There is space after every star.
+
+# Pattern for N = 7
+# *
+#  * *
+#    * * *
+#      * * * *
+#    * * *
+#  * *
+# *
+
+# Read input as specified in the question.
+## Print output as specified in the question.
+n=int(input())
+n1=(n+1)//2
+n2=n//2
+i=0
+while i<n1:
+    s=n1
+    while s>n1-i and n1-i!=0:
+        print(' ',end="")
+        s=s-1
+    j=1
+    while j<=i+1:
+        print('*',end=" ")
+        j=j+1
+    print()
+    i=i+1
+i=n2
+while i>=1:
+    s=1
+    while s<=i-1:
+        print(' ',end="")
+        s=s+1
+    j=1
+    while j<=i:
+        print('*',end=" ")
+        j=j+1
+    print()
+    i=i-1
