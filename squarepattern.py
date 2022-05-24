9b02e887bcc2c6b372bcf700966b079e1b4d5000
Shamanth S Shetty <86283625+Shamanthshetty7@users.noreply.github.com>
2022-05-24 14:31:46 +0530

    Update squarepattern.py

--- squarepattern.py
+++ squarepattern.py
@@ -26,4 +26,4 @@ while i<=n:                  # This for printing the nomber of rows
     print()
     i=i+1
 
-# Try to understand the logic behind the code.Analyze each codes.
+# Try to understand the logic behind the code.Analyze each codes..
