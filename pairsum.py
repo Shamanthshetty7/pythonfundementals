18d4f30998426fa316b4ef28b4527e391542545c
Shamanthshetty7 <shamanthshetty51@gmail.com>
2022-05-30 13:22:10 +0530

    Pair Sum

---
+++ pairsum.py
@@ -0,0 +1,36 @@
+# Pair Sum
+
+
+from sys import stdin
+
+
+def pairSum(arr, n, x) :
+    #Your code goes here
+    count=0
+    for i in range(n):
+        k=i+1
+        for j in range (k,n):
+            if (arr[i]+arr[j]==x):
+                count+=1
+    return count
+
+# Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().strip())
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr, n
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    x = int(stdin.readline().strip())
+    print(pairSum(arr, n, x))
+
+    t -= 1
